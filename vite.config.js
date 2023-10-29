f0c60c330b8e404744c29224170f9dbe2a957f5c
kenny <s1710931008@gms.nutc.edu.tw>
2023-10-29 08:19:04 +0800

    配置

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,39 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// 引入插件Element
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    // 配置插件Element
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [
+        // 配置elementPlus採用sass樣式配色系統
+        ElementPlusResolver({ importStyle: "sass" })
+      ],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css:{
+    preprocessorOptions:{
+      scss:{
+        //自動導入自定制樣式文件
+        additionalData:`
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
