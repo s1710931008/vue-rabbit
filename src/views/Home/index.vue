576645d557152cf73045b4c20246d8aa94847360
kenny <s1710931008@gms.nutc.edu.tw>
2023-10-29 22:51:39 +0800

    Homepanel

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from "./components/HomeBanner.vue";
 import HomeNew from "./components/HomeNew.vue";
 import HomeHot from "./components/HomeHot.vue";
 import homeProduct from "./components/HomeProduct.vue";
+import HomePanel from "./components/HomePanel.vue"
 </script>
 
 <template>
@@ -14,4 +15,17 @@ import homeProduct from "./components/HomeProduct.vue";
   <HomeNew />
   <HomeHot />
   <homeProduct />
+  <!-- 測試面板組件 -->
+  <HomePanel title="新鮮好物" subTitle="新鮮好物 好多品">
+      <div>
+          我是新鮮好物內容
+      </div>
+  </HomePanel>
+
+  <HomePanel title="元氣推件" subTitle="新鮮好物 好多商品">
+      <dir>
+          元氣推件內容
+      </dir>
+  </HomePanel>
+
 </template>
